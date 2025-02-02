8e3009a22613a4c122b3aae99f7946771b65f04d
MichalJaniec <michal.janiec95@gmail.com>
2025-02-02 19:09:50 +0100

    impr project config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,7 @@ import type { Config } from "tailwindcss";
 import typography from '@tailwindcss/typography'; 
 
 const config: Config = {
-    content: [
-        "./app/pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/design-system/**/*.{js,ts,jsx,tsx,mdx}",
-        "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {
             backgroundImage: {
