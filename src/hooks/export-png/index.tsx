ad11490b04b7e2bdb2da3a439d86b8ff26f8496e
MichalJaniec <michal.janiec95@gmail.com>
2025-02-04 00:30:38 +0100

    adjust size for exporting png

--- src/hooks/export-png/index.tsx
+++ src/hooks/export-png/index.tsx
@@ -7,8 +7,30 @@ export const useExportPNG = () => {
 	const handleExport = () => {
 		if (canvasRef.current) {
 			html2canvas(canvasRef.current).then((canvas) => {
+				const desiredWidth = 1080;
+				const desiredHeight = 1350;
+
+				const scaledCanvas = document.createElement("canvas");
+				scaledCanvas.width = desiredWidth;
+				scaledCanvas.height = desiredHeight;
+				const ctx = scaledCanvas.getContext("2d");
+
+				if (ctx) {
+					ctx.drawImage(
+						canvas,
+						0,
+						0,
+						canvas.width,
+						canvas.height,
+						0,
+						0,
+						desiredWidth,
+						desiredHeight,
+					);
+				}
+
 				const link = document.createElement("a");
-				link.href = canvas.toDataURL("image/png");
+				link.href = scaledCanvas.toDataURL("image/png");
 				link.download = "graphic.png";
 				link.click();
 			});
