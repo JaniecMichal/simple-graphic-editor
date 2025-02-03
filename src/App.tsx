8a8190421a248b9262ab08028ef2ad9fffb3b75e
MichalJaniec <michal.janiec95@gmail.com>
2025-02-03 22:49:31 +0100

    add TextProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import React from "react";
 
 import { GraphicEditor } from "./components/editor";
 import { BackgroundProvider } from "./context/background-context";
+import { TextProvider } from "./context/text-context";
 
 function App() {
 	return (
 		<BackgroundProvider>
-			<div className="w-full p-8 pb-0">
-				<GraphicEditor />
-			</div>
+			<TextProvider>
+				<div className="w-full p-8 pb-0">
+					<GraphicEditor />
+				</div>
+			</TextProvider>
 		</BackgroundProvider>
 	);
 }
