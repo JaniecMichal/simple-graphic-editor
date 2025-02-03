1310430c9f592c6803d6435b7b71db891ab15305
MichalJaniec <michal.janiec95@gmail.com>
2025-02-03 22:49:40 +0100

    improving styling

--- src/components/action-tile/index.tsx
+++ src/components/action-tile/index.tsx
@@ -6,21 +6,21 @@ type ActionTileProps = {
 	onClick?: () => void;
 	children?: React.ReactNode;
 };
+
 export const ActionTile = ({ iconSrc, label, onClick, children }: ActionTileProps) => {
 	return (
-		<div className="p-4shadow-md relative h-[256px] w-full cursor-pointer rounded-lg bg-gray-50 pt-16 hover:bg-gray-100">
-			<button
-				className="flex h-full w-full flex-col items-center justify-center gap-6"
-				onClick={onClick}
-			>
-				<img src={iconSrc} alt={label} className="h-24 w-24" />
-
+		<div
+			className="relative h-[256px] w-full cursor-pointer rounded-lg bg-gray-50 pt-16 shadow-md hover:bg-gray-100"
+			onClick={onClick}
+		>
+			<div className="flex h-full w-full flex-col items-center justify-center gap-6">
+				{iconSrc && <img src={iconSrc} alt={label} className="h-24 w-24" />}
 				<span className="pb-[12px] text-center text-lg font-medium text-gray-700">{label}</span>
+			</div>
 
-				{children && (
-					<div className="absolute inset-0 flex items-center justify-center">{children}</div>
-				)}
-			</button>
+			{children && (
+				<div className="absolute inset-0 flex items-center justify-center">{children}</div>
+			)}
 		</div>
 	);
 };
