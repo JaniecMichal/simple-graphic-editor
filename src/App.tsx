75b090adb4d495f4517b2e5880233bf60760f14d
MichalJaniec <michal.janiec95@gmail.com>
2025-02-03 23:44:24 +0100

    add image provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import React from "react";
 import { GraphicEditor } from "./components/editor";
 import { BackgroundProvider } from "./context/background-context";
 import { TextProvider } from "./context/text-context";
+import { ImageProvider } from "./context/image-context";
 
 function App() {
 	return (
 		<BackgroundProvider>
 			<TextProvider>
-				<div className="w-full p-8 pb-0">
-					<GraphicEditor />
-				</div>
+				<ImageProvider>
+					<div className="w-full p-8 pb-0">
+						<GraphicEditor />
+					</div>
+				</ImageProvider>
 			</TextProvider>
 		</BackgroundProvider>
 	);
