6a29c164d77903d825c423b180dc80f893d3d1ff
MichalJaniec <michal.janiec95@gmail.com>
2025-02-03 23:44:10 +0100

    small fixes

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -9,7 +9,7 @@ export const LogoHeader = () => {
 				className="mr-2 h-8 w-8"
 				style={{ filter: "grayscale(100%)", color: "#7209B7" }}
 			/>
-			<h2 className="text-xl font-bold text-gray-600">CanvasEditor</h2>
+			<h1 className="text-xl font-bold text-gray-600">CanvasEditor</h1>
 		</div>
 	);
 };
