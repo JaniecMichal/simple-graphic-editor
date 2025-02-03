f6cb995c8cf53a05f9026310de653fd1db756f15
MichalJaniec <michal.janiec95@gmail.com>
2025-02-03 22:48:21 +0100

    impr config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwind from "tailwindcss";
 import autoprefixer from "autoprefixer";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwind, autoprefixer],
-    }
-  }
-})
+	plugins: [react(), tsconfigPaths()],
+	css: {
+		postcss: {
+			plugins: [tailwind, autoprefixer],
+		},
+	},
+});
