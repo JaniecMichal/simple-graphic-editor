f0c633a6e5c179649927e899715f7d7f94e864dd
MichalJaniec <michal.janiec95@gmail.com>
2025-02-04 09:45:25 +0100

    impr logo size

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -3,8 +3,8 @@ import Logo from "@/assets/logo.svg";
 export const LogoHeader = () => {
 	return (
 		<div className="mb-4 flex items-center">
-			<Logo className="mr-2 h-8 w-8" style={{ fill: "#7209B7" }} />
-			<h1 className="text-xl font-bold text-gray-600">CanvasEditor</h1>
+			<Logo className="mr-2 h-12 w-12" style={{ fill: "#7209B7" }} />
+			<h1 className="text-[32px] font-bold text-gray-600">CanvasEditor</h1>
 		</div>
 	);
 };
